fedda14e95fba7ac0c83a025f675f217ae78912f
Alyssa Drobatz <drobatzalyssa@gmail.com>
2017-04-05 21:54:23 -0400

    error and css cleanup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
     <h1>Scrabble Word Score Calculator</h1>
     <p>Want to figure out what points you could get with the scrabble word you built?</p>
     <p>Click the button below to figure it out!</p>
+    <p>Good luck!</p>
 <!-- enter the app -->
-    <button class="button"><a href="/calculate"> Calculate my Score!</a></button>
+    <div><a href="/calculate"> Calculate my Score >></a></div>
 
 @endsection
